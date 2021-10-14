8f3eb5cd8a29de4f396d5ff9d9a17ed88bf06a26
Danny <danny@Dannys-MacBook-Pro.local>
2021-10-14 09:28:29 +0700

    commit mac

--- main.dart
+++ main.dart
@@ -2,22 +2,17 @@ class Student {
   final String name;
   final int age;
 
-  Student({
-    this.name = "", 
-    this.age = 0
-  });
+  Student({this.name = "", this.age = 0});
 
-  String toString(){
+  String toString() {
     return '{ ${this.name}, ${this.age} }';
   }
 }
 
-void main(){
-  List<Student> myStudent = [
-    Student(name: 'Agus', age: 15)
-  ];
-  myStudent.add(Student(name: "Budi", age: 12));
-  myStudent.add(Student(name: "Charlie", age: 18));
+void main() {
+  List<Student> myStudent = [Student(name: 'Agus 1', age: 15)];
+  myStudent.add(Student(name: "Budi 2", age: 12));
+  myStudent.add(Student(name: "Charlie 3", age: 18));
 
   for (var i = 0; i < myStudent.length; i++) {
     print(myStudent[i]);
